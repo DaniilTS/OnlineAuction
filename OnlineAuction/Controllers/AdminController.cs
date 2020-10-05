4ed667a471f3dc808b3765541640cb6d1e7bbe9a
DaniilTS <48275635+DaniilTS@users.noreply.github.com>
2020-10-05 03:23:24 +0300

    Added Admin Panel and CRUD for Users there

---
+++ OnlineAuction/Controllers/AdminController.cs
@@ -0,0 +1,110 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using OnlineAuction.Models;
+using OnlineAuction.ViewModels;
+
+namespace OnlineAuction.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<User> _userManager;
+
+        public AdminController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Users() => View(_userManager.Users.ToList());
+        
+        public IActionResult CreateUser() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    Email = model.Email, 
+                    UserName = model.Name, 
+                    Year = model.Year
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Users");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> EditUser(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            
+            EditUserViewModel model = new EditUserViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Year = user.Year
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUser(EditUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.Email = model.Email;
+                    user.UserName = model.Name;
+                    user.Year = model.Year;
+
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Users");
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+            }
+
+            return RedirectToAction("Users");
+        }
+    }
+}
---
+++ OnlineAuction/ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineAuction.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Year { get; set; }
+    }
+}
