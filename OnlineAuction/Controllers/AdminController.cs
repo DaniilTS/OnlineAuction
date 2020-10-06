2dc4a92efdb98f6ff4f952a09dcecbd4af601f31
DaniilTS <48275635+DaniilTS@users.noreply.github.com>
2020-10-06 19:09:31 +0300

    Added Logging

--- OnlineAuction/Controllers/AdminController.cs
+++ OnlineAuction/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +9,7 @@ using OnlineAuction.ViewModels;
 
 namespace OnlineAuction.Controllers
 {
+    
     public class AdminController : Controller
     {
         private UserManager<User> _userManager;
--- OnlineAuction/Program.cs
+++ OnlineAuction/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace OnlineAuction
 {
@@ -13,11 +15,23 @@ namespace OnlineAuction
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.File("Logs\\all-logs.txt")
+                .WriteTo.Logger(lc=> lc
+                    .Filter.ByIncludingOnly(le=> le.Level == LogEventLevel.Error)
+                    .WriteTo.File("Logs\\error-logs.txt"))
+                .WriteTo.Logger(lc=>lc
+                    .Filter.ByIncludingOnly(le=>le.Level == LogEventLevel.Error)
+                    .WriteTo.Console())
+                .CreateLogger();
+            
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
